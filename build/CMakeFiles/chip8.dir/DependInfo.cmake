695d1fa91ee7346b58005e1e7f82606bb120efd5
RafaelCasamaximo <rafael.furlanetto@uel.br>
2022-03-13 10:44:48 -0300

    Terminando implementação básica

---
+++ build/CMakeFiles/chip8.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/rafaelfc/Documents/CHIP8-Emulator/src/chip8.cpp" "CMakeFiles/chip8.dir/src/chip8.cpp.o" "gcc" "CMakeFiles/chip8.dir/src/chip8.cpp.o.d"
+  "/home/rafaelfc/Documents/CHIP8-Emulator/src/main.cpp" "CMakeFiles/chip8.dir/src/main.cpp.o" "gcc" "CMakeFiles/chip8.dir/src/main.cpp.o.d"
+  "/home/rafaelfc/Documents/CHIP8-Emulator/src/platform.cpp" "CMakeFiles/chip8.dir/src/platform.cpp.o" "gcc" "CMakeFiles/chip8.dir/src/platform.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
