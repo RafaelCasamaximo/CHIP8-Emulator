ba5c24bd28622cacb50d14feff212f7772b499f4
RafaelCasamaximo <rafael.furlanetto@uel.br>
2022-03-13 10:05:24 -0300

    Implementando OPCodes

--- src/chip8.hpp
+++ src/chip8.hpp
@@ -58,11 +58,50 @@ public:
     // A resolução é 64x32
     // uint32_t para facilitar a comunicação com SDL
     uint32_t video[64 * 32]{};
+    const unsigned int VIDEO_WIDTH = 64;
+    const unsigned int VIDEO_HEIGHT = 32;
     // Responsável por guardar o opcode atual
     uint16_t opcode{};
+    // Responsável pelos números random
+    std::default_random_engine randGen;
+    std::uniform_int_distribution<uint8_t> randByte;
 
     // MÉTODOS
     Chip8();
     void LoadROM(char const* filename);
+    void OP_00E0();
+    void OP_00EE();
+    void OP_1nnn();
+    void OP_2nnn();
+    void OP_3xkk();
+    void OP_4xkk();
+    void OP_5xy0();
+    void OP_6xkk();
+    void OP_7xkk();
+    void OP_8xy0();
+    void OP_8xy1();
+    void OP_8xy2();
+    void OP_8xy3();
+    void OP_8xy4();
+    void OP_8xy5();
+    void OP_8xy6();
+    void OP_8xy7();
+    void OP_8xyE();
+    void OP_9xy0();
+    void OP_Annn();
+    void OP_Bnnn();
+    void OP_Cxkk();
+    void OP_Dxyn();
+    void OP_Ex9E();
+    void OP_ExA1();
+    void OP_Fx07();
+    void OP_Fx0A();
+    void OP_Fx15();
+    void OP_Fx18();
+    void OP_Fx1E();
+    void OP_Fx29();
+    void OP_Fx33();
+    void OP_Fx55();
+    void OP_Fx65();
 };
 
